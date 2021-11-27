f5f45c0dd1ae62522b5c28d6932cfa305944255d
Lane <89763058+CptR3dBeard@users.noreply.github.com>
2021-11-27 10:51:04 +1000

    Updated Front End
    NA

--- MongoDBAnalytics/app/main.py
+++ MongoDBAnalytics/app/main.py
@@ -19,14 +19,14 @@ def LinearR():
     """This function LinearR queries the MongoDB for the collection.
     In this example; the collection contains 2019/2020 12month Stock prices of MSFT,
     these entries will then undergo linear regression."""
-    x =col.find({}, {'_id': 0})   # variable containing pymongo find command aswell as removing the _id 
-    for i in x:                   # iterating over x variables results
-        df = pd.DataFrame(x)      # this uses pandas on our query to turn all the data/columns into a dataframe
+    x =col.find({}, {'_id': 0})                  # variable containing pymongo find command aswell as removing the _id 
+    for i in x:                                  # iterating over x variables results
+        df = pd.DataFrame(x)                     # this uses pandas on our query to turn all the data/columns into a dataframe
         df.reset_index(drop=True, inplace=True)  # dropping index and modifying data inplace ( will not move)
-        X = df.iloc[:, :-1].values  # use location based indexing on 2 columns to determine x axis
-        y= df.iloc[:, 1].values     # use location based indexing on 2 columns to determine y axis
-        lr.fit(X, y)                # we are now performing linear regression on the data supplied.
-        return np.ndarray.tolist(lr.predict(X)) # Converting the X value of linear prediction to a list returning to FastAPI
+        X = df.iloc[:, :-1].values               # use location based indexing on 2 columns to determine x axis
+        y= df.iloc[:, 1].values                  # use location based indexing on 2 columns to determine y axis
+        lr.fit(X, y)                             # we are now performing linear regression on the data supplied.
+        return np.ndarray.tolist(lr.predict(X))  # Converting the X value of linear prediction to a list returning to FastAPI
 
 def Ridge_Regression():
     """This function performs Ridge Regression fomr the MongoDB collection.
@@ -34,17 +34,17 @@ def Ridge_Regression():
     this data undergoes ridge regression."""
     x =col.find({}, {'_id': 0})
     for i in x:
-        df = pd.DataFrame(x)        # here we define out dataframe as a variable DF
-        df.reset_index(drop=True, inplace=True)     # reseting & dropping the index of Panda datafrom so Sklearn can process
-        X = df.iloc[:, :-1].values  # use location based indexing on 2 columns to determine x axis
-        y= df.iloc[:, 1].values     # use location based indexing on 2 columns to determine y axis
+        df = pd.DataFrame(x)                     # here we define out dataframe as a variable DF
+        df.reset_index(drop=True, inplace=True)  # reseting & dropping the index of Panda datafrom so Sklearn can process
+        X = df.iloc[:, :-1].values               # use location based indexing on 2 columns to determine x axis
+        y= df.iloc[:, 1].values                  # use location based indexing on 2 columns to determine y axis
         rig.fit(X, y)
-        return np.ndarray.tolist(rig.predict(X))    # convert numpy array into a list for ridge prediction
+        return np.ndarray.tolist(rig.predict(X)) # convert numpy array into a list for ridge prediction
 
 def insert_new_dataset(file_name):
     """This function allows the user to insert a complete new dataset into a MongoDB collection
     The data can be anything and it will upload to the localhost database for MongoDB"""
-    df = pd.read_csv(file_name)                     # Reading the uploaded file contents as CSV
-    data = df.to_dict(orient="records")             # Converting the dataframe to a dictionary
-    col.insert(data)                                # Executing the collection insert function using the converted dataframe
-    return{'Test':'Successful'}                     # Return successful test to API
+    df = pd.read_csv(file_name)                  # Reading the uploaded file contents as CSV
+    data = df.to_dict(orient="records")          # Converting the dataframe to a dictionary
+    col.insert(data)                             # Executing the collection insert function using the converted dataframe
+    return{'Test':'Successful'}                  # Return successful test to API
